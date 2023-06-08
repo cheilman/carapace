dd99e858e0c52c8fc5a09cab9b641db5aa388d42
Chris Heilman <heilmanc@amazon.com>
2023-06-08 16:41:31 -0400

    Enforcing sort order to make future changes clearer.

--- prereqs/get-brew.sh
+++ prereqs/get-brew.sh
@@ -3,8 +3,8 @@
 # Writes brew installed packages to a bunch of files
 #
 
-brew list --cask > brew-casks
-brew list --formulae -tr -1 > brew-formulae
-brew leaves --installed-on-request > brew-leaves-installed-on-request
-brew leaves --installed-on-request | xargs brew desc --eval-all > brew-leaves-installed-on-request-with-desc
+brew list --cask | sort > brew-casks
+brew list --formulae -tr -1 | sort > brew-formulae
+brew leaves --installed-on-request | sort > brew-leaves-installed-on-request
+brew leaves --installed-on-request | xargs brew desc --eval-all | sort > brew-leaves-installed-on-request-with-desc
 
