818731237e1f8bd37ec13b673b502fd7f5f09b7b
Chris Heilman <heilmanc@amazon.com>
2023-09-01 16:06:18 -0400

    Why did I write this twice?

--- prereqs/get-brew.sh
+++ prereqs/get-brew.sh
@@ -1,10 +1,13 @@
-#!bash
+#!/bin/bash
 #
-# Writes brew installed packages to a bunch of files
+# Gather installed information
 #
 
 brew list --cask | sort > brew-casks
+
 brew list --formulae -tr -1 | sort > brew-formulae
+
 brew leaves --installed-on-request | sort > brew-leaves-installed-on-request
-brew leaves --installed-on-request | xargs brew desc --eval-all | sort > brew-leaves-installed-on-request-with-desc
+brew leaves --installed-on-request | sort | xargs brew desc --eval-all > brew-leaves-installed-on-request-with-desc
+
 
