c0852110b9d55b457a252a6d5512f84477348718
Chris Heilman <chris.heilman@gmail.com>
2024-05-30 10:12:38 -0400

    Getting date/time in a single command, rather than two.

--- bundles/base/45tmux/config/tmux-powerline/config.sh
+++ bundles/base/45tmux/config/tmux-powerline/config.sh
@@ -66,7 +66,7 @@
 
 # date.sh {
 	# date(1) format for the date. If you don't, for some reason, like ISO 8601 format you might want to have "%D" or "%m/%d/%Y".
-	export TMUX_POWERLINE_SEG_DATE_FORMAT="%Y-%m-%d"
+	export TMUX_POWERLINE_SEG_DATE_FORMAT="%Y-%m-%d %H:%M"
 # }
 
 # date_week.sh {
